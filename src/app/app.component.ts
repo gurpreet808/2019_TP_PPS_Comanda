b9e16f8041e990f97c8bc32c2377b90d09ecdb42
gurpreet808 <gurpreets808@gmail.com>
2019-05-30 01:56:09 -0300

    Agrego angularfire y logica de menu para usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,78 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+//import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { UsuarioService } from './servicios/usuario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
-  ];
+  public splash = true;
+
+  public appPages = [];
 
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    //private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private servUsuario: UsuarioService
   ) {
     this.initializeApp();
+    setTimeout(() => this.splash = false, 5000);
+    this.servUsuario.logueado.subscribe(
+      valor =>{
+        if (valor == false) {
+          this.appPages = [
+            {
+              title: 'Iniciar sesión',
+              url: '/login',
+              icon: 'log-in'
+            },
+            {
+              title: 'Registrarse',
+              url: '/registrarse',
+              icon: 'list'
+            }
+          ];
+        } else {
+          this.appPages = [
+            {
+              title: 'Inicio',
+              url: '/',
+              icon: 'home'
+            },
+            {
+              title: 'Tabla',
+              url: '/tabla',
+              icon: 'happy'
+            }
+          ];
+        }
+      }
+    );
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
     });
+  }
+
+  estilo_app(): object{
+    if(this.splash){
+      let estilo_splash = {
+        "background-color": "#6bfc77"
+      };
+
+      return estilo_splash;
+    }
+    return {};
+  }
+
+  salir(){
+    //this.servUsuario.cerrar_sesion();
   }
 }
