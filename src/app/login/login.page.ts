090a00b161a23e3476fa4e2fe533ba08dc72b9e5
gurpreet808 <gurpreets808@gmail.com>
2019-06-03 02:10:33 -0300

    Agrego lista de usuarios de prueba

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,7 @@ import { AngularFireDatabase } from '@angular/fire/database';
 import { UsuarioService } from '../servicios/usuario.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -15,14 +16,16 @@ export class LoginPage implements OnInit {
   showpass: boolean = false;
   
   usuariosTest = [
-    {correo: "admin@gmail.com", clave: "1111"},
-    {correo: "invitado@gmail.com", clave: "2222"},
-    {correo: "usuario@gmail.com", clave: "3333"},
-    {correo: "anonimo@gmail.com", clave: "4444"},
-    {correo: "tester@gmail.com", clave: "5555"}
+    {correo: "supervisor@supervisor.com", clave: "supervisor.lc2019"},
+    {correo: "empleado@empleado.com", clave: "empleado.lc2019"},
+    {correo: "mozo@mozo.com", clave: "mozo.lc2019"},
+    {correo: "cocinero@cocinero.com", clave: "cocinero.lc2019"},
+    {correo: "bartender@bartender.com", clave: "bartender.lc2019"},
+    {correo: "cliente@cliente.com", clave: "cliente.lc2019"},
+    {correo: "dueño@dueño.com", clave: "dueño.lc2019"}
   ];
 
-  constructor(db: AngularFireDatabase, public servUsuario: UsuarioService, private formBuilder: FormBuilder, public router:Router) {
+  constructor(db: AngularFireDatabase, public servUsuario: UsuarioService, private formBuilder: FormBuilder, public router:Router, public actionSheetCtrl: ActionSheetController) {
     this.loginForm = this.formBuilder.group({
       correo: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'), Validators.required])],
       clave: ['', Validators.compose([Validators.maxLength(20), Validators.required])],
@@ -46,6 +49,26 @@ export class LoginPage implements OnInit {
   elegirTest(id:number){
     this.loginForm.controls['correo'].setValue(this.usuariosTest[id].correo);
     this.loginForm.controls['clave'].setValue(this.usuariosTest[id].clave);
+  }
+
+  async presentActionSheet() {
+    let controles = [];
+
+    for (let index = 0; index < this.usuariosTest.length; index++) {
+      controles.push({
+        text: this.usuariosTest[index].correo,
+        cssClass: 'centerActionSheet',
+        handler: () => {
+          this.elegirTest(index);
+        }
+      });
+    }
+    
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Usuarios de prueba',
+      buttons: controles
+    });
+    await actionSheet.present();
   }
 
   registrarme(){
